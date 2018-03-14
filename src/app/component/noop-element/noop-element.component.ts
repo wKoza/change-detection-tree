3ba642750938a3907dbd61e63a687aa5370f4f73
mivanov <mivanov@cinimex.ru>
2018-03-14 18:28:21 +0300

    feat(app): add example without zone

--- src/app/component/noop-element/noop-element.component.ts
+++ src/app/component/noop-element/noop-element.component.ts
@@ -1,11 +1,12 @@
-import {Component, Injector} from '@angular/core';
+import {ChangeDetectionStrategy, Component, Injector} from '@angular/core';
 import {Noop} from "./noop.class";
 import {Lifecycle} from "../../utils/Lifecycle";
 
 @Component({
   selector: 'noop-element',
   templateUrl: './noop-element.component.html',
-  styleUrls: ['./noop-element.component.css']
+  styleUrls: ['./noop-element.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 @Lifecycle()
 export class NoopElementComponent extends Noop {
